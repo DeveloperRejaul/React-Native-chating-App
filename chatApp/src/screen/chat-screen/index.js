9a6a1760255eea0336e988066706e6854f6b98fd
rejaul <devrejaul.official@gmail.com>
2023-04-08 22:28:53 +0600

    add

--- chatApp/src/screen/chat-screen/index.js
+++ chatApp/src/screen/chat-screen/index.js
@@ -1,14 +1,9 @@
 import {StyleSheet, View} from 'react-native';
 import React from 'react';
 import ChatUI from './ChatUI';
-import {LastMessageProvider} from '../../context/LastMessageContext';
 
 export default function ChatUiIndex({route}) {
-  return (
-    <LastMessageProvider>
-      <ChatUI route={route} />
-    </LastMessageProvider>
-  );
+  return <ChatUI route={route} />;
 }
 
 const styles = StyleSheet.create({});
--- chatApp/src/screen/main-screen/index.js
+++ chatApp/src/screen/main-screen/index.js
@@ -1,14 +1,6 @@
-import {StyleSheet} from 'react-native';
 import React from 'react';
 import MainScreen from './MainScreen';
-import {LastMessageProvider} from '../../context/LastMessageContext';
 
 export default function MainScreenIndex() {
-  return (
-    <LastMessageProvider>
-      <MainScreen />
-    </LastMessageProvider>
-  );
+  return <MainScreen />;
 }
-
-const styles = StyleSheet.create({});
