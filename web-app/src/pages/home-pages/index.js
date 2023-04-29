28d533b47df656f53b7c337482dce9b2250438e5
rejaul <devrejaul.official@gmail.com>
2023-04-29 15:01:05 +0600

    add

--- web-app/src/pages/home-pages/index.js
+++ web-app/src/pages/home-pages/index.js
@@ -3,13 +3,24 @@ import "../../app.css";
 import { useNavigate } from "react-router-dom";
 import Button from "../../components/button/Button";
 import { NAV_PHAT } from "../../constants/NAV_PHAT";
+import { Box } from "@chakra-ui/react";
 
 export default function HomePage() {
   const navigate = useNavigate();
 
   return (
     <div className="container">
-      <div className="innerContainer">
+      <Box
+        height={["100%", "100%", "90%"]}
+        display={["flex"]}
+        bg={"#ecf2ff"}
+        borderRadius={"md"}
+        width={["100%", "100%", "50%"]}
+        flexDirection={"column"}
+        p={"20px"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
         <Button
           text="Login"
           style={{ backgroundColor: "#62cdff", width: "60%" }}
@@ -20,7 +31,7 @@ export default function HomePage() {
           style={{ backgroundColor: "#62cdff", width: "60%" }}
           onClick={() => navigate(NAV_PHAT.SIGNUP)}
         />
-      </div>
+      </Box>
     </div>
   );
 }
